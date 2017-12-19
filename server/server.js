f4fa3d49dfad267247f4e890ae8d74f917471072
claire <clairemstolp@gmail.com>
2017-12-18 21:35:05 -0500

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -34,7 +34,6 @@ app.get('/todos', (req, res) =>{
   });
 });
 
-// Get /todos/o438q5798
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)){
@@ -45,6 +44,22 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
     res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(todo);
   }).catch((e) => {
     res.status(400).send();
   });
